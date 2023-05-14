84e80fd53f822ee5c3af5670b0f5ea70555179b9
Your Name <henry.mickael1@gmail.com>
2023-05-14 18:45:45 +0200

    correctif maj, mais bug tjs présent

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const userCtrl = require("../controller/User");
+const userCtrl = require("../controller/user");
 
 router.post("/signup", userCtrl.signup);
 router.post("/login", userCtrl.login);
